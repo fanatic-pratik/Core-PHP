4c666ecc0e9ceafbedc2dd3fe4ef95c1392071cd
fanatic-pratik <pratikdhamal6@gmail.com>
2026-02-02 23:58:07 +0530

    login page using session

--- session/login.php
+++ session/login.php
@@ -20,7 +20,7 @@ if($name==='pratik'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Page</title>
 </head>
 <body>
     <form action="" method="post" enctype="multipart/form-data">
